8938fd68815b69f99673c05aa0a9b2e251507d71
Eric Simmons <shan.simmons@gmail.com>
2026-03-08 03:50:42 -0500

    fix css

--- src/content.config.ts
+++ src/content.config.ts
@@ -13,13 +13,13 @@ const lore = defineCollection({
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
+		heroImage: image().optional(),
 	}),
 });
 
